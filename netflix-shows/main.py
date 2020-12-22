d705eca8972e38fde369620ea494efc8ee7842d2
Jubin Soni <jubin@chartmetric.com>
2020-12-22 01:39:30 -0800

    Fixed dev env

--- netflix-shows/main.py
+++ netflix-shows/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from . import models, database, crud
+import models, database, crud
 import uvicorn
 
 # Create Connection
